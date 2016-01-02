c9e2e9a4ac0a765f75697be918f258bb70263c53
Louis-André Labadie <monsieur@louis-andre.net>
2016-01-02 18:26:32 -0500

    Simplify components hierarchy, discard overkill routing

--- app/index.js
+++ app/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router } from 'react-router';
-import routes from './routes';
-import configureStore from './store/configureStore';
+import Renderer from './components/Renderer';
 import css from './assets/css/style.scss';
 
-const store = configureStore();
-
 render(
-  <Provider store={store}>
-    <Router>
-      {routes}
-    </Router>
-  </Provider>,
+  <Renderer />,
   document.getElementById('root')
 );
-
-if (process.env.NODE_ENV !== 'production') {
-  // Use require because imports can't be conditional.
-  // In production, you should ensure process.env.NODE_ENV
-  // is envified so that Uglify can eliminate this
-  // module and its dependencies as dead code.
-  // require('./createDevToolsWindow')(store);
-}
