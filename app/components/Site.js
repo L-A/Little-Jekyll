96246362e9f35928b8d188d36c353400e6662171
Louis-André Labadie <monsieur@louis-andre.net>
2016-01-19 10:04:03 -0500

    Add options bar, divide stylesheets

--- app/components/Site.js
+++ app/components/Site.js
@@ -3,11 +3,18 @@ import Dispatcher from '../utils/front-end-dispatcher';
 import shell from 'shell';
 
 var Site = React.createClass({
+  getInitialState: function() {
+    return {optionsShown: false};
+  },
   toggleServerState: function() {
     var message = this.props.siteInfo.serverActive ? 'stopServer' : 'startServer';
     if( !this.props.siteInfo.serverRequested ) {
       Dispatcher.send(message, this.props.siteInfo.id);
     }
+  },
+  toggleOptionsPanel: function() {
+    var newOptionsState = !this.state.optionsShown;
+    this.setState({optionsShown: newOptionsState});
   },
   openLocalServer: function() {
     if(this.props.siteInfo.serverActive) {
@@ -17,23 +24,35 @@ var Site = React.createClass({
   openFolder: function() {
     shell.openItem(this.props.siteInfo.filePath);
   },
+  removeSiteFromList: function() {
+  },
+  buildSite: function() {
+  },
   render: function () {
     var siteInfo = this.props.siteInfo;
+    var cellClass = this.state.optionsShown ? "site-cell options-shown" : "site-cell";
     var switchState = 'site-serve-switch ' + (siteInfo.serverActive ? 'switch-on' : (siteInfo.serverRequested ? 'switch-working' : 'switch-off'));
     return (
-      <li className="site-cell">
-        <a className={switchState} onClick={this.toggleServerState}>
-          <div className="groove">
-            <div className="knob"></div>
+      <li className={cellClass}>
+        <div className="main-panel">
+          <a className={switchState} onClick={this.toggleServerState}>
+            <div className="groove">
+              <div className="knob"></div>
+            </div>
+          </a>
+          <div className="site-info">
+            <h1 className={siteInfo.serverActive ? 'server-active' : ''}>{siteInfo.name}</h1>
+            <p className="site-folder" onClick={this.openFolder}>{siteInfo.filePath}</p>
           </div>
-        </a>
-        <div className="site-info">
-          <h1 className={siteInfo.serverActive ? 'server-active' : ''}>{siteInfo.name}</h1>
-          <p className="site-folder" onClick={this.openFolder}>{siteInfo.filePath}</p>
+          <div className="site-options">
+            <a className={siteInfo.serverActive ? 'btn-preview available' : 'btn-preview'} onClick={this.openLocalServer}></a>
+            <a className="btn-edit" onClick={this.toggleOptionsPanel}></a>
+          </div>
         </div>
-        <div className="site-options">
-          <a className="btn-edit"></a>
-          <a className={siteInfo.serverActive ? 'btn-preview available' : 'btn-preview'} onClick={this.openLocalServer}></a>
+        <div className="secondary-panel">
+          <a className="btn-remove" onClick={this.removeSiteFromList}></a>
+          <a className="btn-build" onClick={this.buildSite}></a>
+          <a className="btn-edit" onClick={this.toggleOptionsPanel}></a>
         </div>
       </li>
     );
