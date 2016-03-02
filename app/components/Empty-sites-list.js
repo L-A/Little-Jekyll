77b61a30514bee4bc39fc59c3b1fd6759c5807af
Louis-André Labadie <monsieur@louis-andre.net>
2016-03-02 13:29:30 -0500

    Fix empty window on very first open

--- app/components/Empty-sites-list.js
+++ app/components/Empty-sites-list.js
@@ -10,26 +10,22 @@ var EmptySitesList = React.createClass({
     Dispatcher.send('createSite');
   },
   render: function () {
-    if(this.props.sitesReceived == true) {
-      var childIcon = <div className="icon" />;
-      return (
-        <div className="empty-sites-list">
-          <p>Oh dear! This list is empty.</p>
-          <div className="buttons-row">
-            <SimpleButton onClick={this.createNewSite} className="btn-create">
-              <div className="icon" />
-              <span>Create</span>
-            </SimpleButton>
-            <SimpleButton href="#" onClick={this.requestNewSite} className="btn-open">
-              <div className="icon" />
-              <span>Open</span>
-            </SimpleButton>
-          </div>
+    var childIcon = <div className="icon" />;
+    return (
+      <div className="empty-sites-list">
+        <p>Oh dear! This list is empty.</p>
+        <div className="buttons-row">
+          <SimpleButton onClick={this.createNewSite} className="btn-create">
+            <div className="icon" />
+            <span>Create</span>
+          </SimpleButton>
+          <SimpleButton href="#" onClick={this.requestNewSite} className="btn-open">
+            <div className="icon" />
+            <span>Open</span>
+          </SimpleButton>
         </div>
-      );
-    } else {
-      return ( <div className="empty-sites-list" /> );
-    }
+      </div>
+    );
   }
 })
 
